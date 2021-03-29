625697acfb58333090e704ca0c0fb7661c43d091
YaroslavLys <mryaroslav337@gmail.com>
2021-03-29 11:29:32 +0300

    Code modified

--- main.py
+++ main.py
@@ -5,15 +5,12 @@ def main():
     input_file = "D:\\Python\\PycharmProjects\\access_log_Jul95"
 
     with open(input_file, mode="r") as f:
-        result1 = re.findall(r"\[01/Jul/1995:01:39:\d\d -0400] \"GET /shuttle/missions/sts-\d\d", f.read())
-    with open(input_file, mode="r") as f:
-        result2 = re.findall(r"\[01/Jul/1995:01:[4-5]\d:\d\d -0400] \"GET /shuttle/missions/sts-\d\d", f.read())
-    with open(input_file, mode="r") as f:
-        result3 = re.findall(r"\[01/Jul/1995:02:\d\d:\d\d -0400] \"GET /shuttle/missions/sts-\d\d", f.read())
-    with open(input_file, mode="r") as f:
-        result4 = re.findall(r"\[01/Jul/1995:03:[0-3]\d:\d\d -0400] \"GET /shuttle/missions/sts-\d\d", f.read())
-    with open(input_file, mode="r") as f:
-        result5 = re.findall(r"\[01/Jul/1995:03:4[0-2]:\d\d -0400] \"GET /shuttle/missions/sts-\d\d", f.read())
+        text = f.read()
+        result1 = re.findall(r"\[01/Jul/1995:01:39:\d\d -0400] \"GET /shuttle/missions/sts-\d\d", text)
+        result2 = re.findall(r"\[01/Jul/1995:01:[4-5]\d:\d\d -0400] \"GET /shuttle/missions/sts-\d\d", text)
+        result3 = re.findall(r"\[01/Jul/1995:02:\d\d:\d\d -0400] \"GET /shuttle/missions/sts-\d\d", text)
+        result4 = re.findall(r"\[01/Jul/1995:03:[0-3]\d:\d\d -0400] \"GET /shuttle/missions/sts-\d\d", text)
+        result5 = re.findall(r"\[01/Jul/1995:03:4[0-2]:\d\d -0400] \"GET /shuttle/missions/sts-\d\d", text)
 
     result = []
     result.extend(result1)
